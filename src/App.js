a4d4b583d735c9140970ddd0a913eeabdbd47eba
madzamador <cyruz082000@yahoo.com>
2023-11-23 16:50:42 +0800

    Update the App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <Job salary={100000} position="Senior SDE" company="Amazon" />
+      <Job salary={90000} position="Junior SDE" company="Google" />
+      <Job salary={80000} position="Project Manager" company="Facebook" />
+    </div>
+  );
+}
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary} </h1>
+      <h2> {props.position} </h2>
+      <h1> {props.company} </h1>
     </div>
   );
 }
